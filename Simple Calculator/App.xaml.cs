dc011979b27497a88960bd42a27d3a6d38f8f713
Brian593 <brian593@live.com>
2024-09-20 14:14:39 -0500

    .

---
+++ Simple Calculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Simple_Calculator;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Simple Calculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Calculator;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
